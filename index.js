6bfb252e41290bb4dfea4006f38126ab8e8604c3
akashnimare <svnitakash@gmail.com>
2015-10-04 12:50:53 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+var github = require('octonode');
+var simpleGit = require('simple-git');
+
+var user_login = {
+  username: process.argv[2],
+  password: process.argv[3]
+};
+var client = github.client(user_login);
+var ghme   = client.me();
+client.post('/user/repos', {}, function (err, status, body, headers) {
+  ghme.repo({
+    "name": process.argv[4]
+  }, function () {
+      require('simple-git')()
+     .init()
+     .add('./*')
+     .commit(process.argv[5])
+     .addRemote('origin', 'git@github.com:' + process.argv[2] + '/' + process.argv[4] + '.git')
+     .push('origin', 'master');
+
+  });
+});
+
+
+
